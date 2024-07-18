62f56e7fafba8a46021b5e7f1ce9b96f07b06e9a
goku31s <168161576+goku31s@users.noreply.github.com>
2024-07-18 12:34:14 -0400

    Update var.tf

--- modules/var.tf
+++ modules/var.tf
@@ -10,4 +10,7 @@ variable "backend_bucket" {
   description = "Name of the Google Cloud Storage bucket to store Terraform state"
 }
 
+variable "environment" {
+  description = "Environment name ('dev' or 'prod')"
+}
 
